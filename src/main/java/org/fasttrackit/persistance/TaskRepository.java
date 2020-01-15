b1074701a59d549b0462195ddb3d041f8e94ac3a
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-01-15 21:27:01 +0200

    added database configuration

---
+++ src/main/java/org/fasttrackit/persistance/TaskRepository.java
@@ -0,0 +1,27 @@
+package org.fasttrackit.persistance;
+
+import org.fasttrackit.transfer.CreateTaskRequest;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TaskRepository {
+
+    public void createTask(CreateTaskRequest request) throws SQLException, IOException {
+
+        //preventing SQL Injection by avoiding concatenation and using PreparedStatement
+        String sql = "INSERT INTO task (description, deadline) VALUES (?,?)";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, request.getDescription());
+            preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+}
