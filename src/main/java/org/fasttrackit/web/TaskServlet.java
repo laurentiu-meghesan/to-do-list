09af8b3178e8001e01bf2680a30e6e936b11f492
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2020-02-05 21:18:03 +0200

    configured CORS

--- src/main/java/org/fasttrackit/web/TaskServlet.java
+++ src/main/java/org/fasttrackit/web/TaskServlet.java
@@ -25,6 +25,8 @@ public class TaskServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        setAccessControlHeaders(resp);
+
         CreateTaskRequest request = ObjectMapperConfiguration.objectMapper
                 .readValue(req.getReader(), CreateTaskRequest.class);
         try {
@@ -37,6 +39,8 @@ public class TaskServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        setAccessControlHeaders(resp);
+
         String id = req.getParameter("id");
         try {
             taskService.deleteTask(Long.parseLong(id));
@@ -47,6 +51,8 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        setAccessControlHeaders(resp);
 
         String id = req.getParameter("id");
         UpdateTaskRequest request = ObjectMapperConfiguration.objectMapper.readValue(req.getReader(),
@@ -62,6 +68,8 @@ public class TaskServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        setAccessControlHeaders(resp);
+
         try {
             List<Task> tasks = taskService.getTasks();
 
@@ -71,5 +79,23 @@ public class TaskServlet extends HttpServlet {
         } catch (SQLException | ClassNotFoundException e) {
             resp.sendError(500, "Internal server error: " + e.getMessage());
         }
+    }
+
+
+//    for pre-flight requests
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        setAccessControlHeaders(resp);
+
+
+    }
+
+//    CORS configuration (Cross-Origin-Resource-Sharing)
+    private void setAccessControlHeaders(HttpServletResponse resp){
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        resp.setHeader("Access-Control-Allow-Headers", "content-type");
     }
 }
